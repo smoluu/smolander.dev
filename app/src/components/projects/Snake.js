919ee9384494ca01b25cceec852c5f20d0eb1da3
smolu <smolunpapu@gmail.com>
2024-02-03 19:04:23 +0200

    snake: mobile touch controls

--- app/src/components/projects/Snake.js
+++ app/src/components/projects/Snake.js
@@ -5,11 +5,14 @@ const Snake = () => {
   useEffect(() => {
     const gameArea = document.getElementById("gameArea");
     const startButton = document.getElementById("startButton");
-    const snakedebug = document.getElementById("snakedebug");
     const snakeIndex = document.getElementById("snakeIndex");
     const snakeScore = document.getElementById("snakeScore");
+    const touchControlArea = document.getElementById("touchControlArea")
+
     const width = 20;
+
     // load audio
+    
     var soundEat = new Audio();
     var soundLose = new Audio();
     soundEat.src = window.location.origin + "/sounds/snake/eat.mp3";
@@ -24,23 +27,31 @@ const Snake = () => {
     let appleIndex;
 
     //create grid
+
     for (var i = 0; i < width * width; i++) {
       gameArea.appendChild(document.createElement("div"));
     }
     const grid = document.querySelectorAll(".gameArea div");
-    grid.forEach((index) => {
-      console.log(grid[index]);
-    });
+
+    // game start
 
     function startGame() {
+      touchControlArea.removeEventListener("click", startGame);
+
+      touchControlArea.removeEventListener("touchstart", startGame)
+      touchControlArea.addEventListener("touchstart",handleTouch)
+
       interval = setInterval(move, intervalTime * speed);
       snake.forEach((index) => grid[index].classList.add("snake"));
       spawnApple();
     }
 
     function move() {
+      console.log(interval)
       snakeIndex.innerHTML = snake[0] + snakeDirection;
+
       //lose condition
+      
       if (
         snake[0] + snakeDirection < 0 ||
         snake[0] + snakeDirection > 399 ||
@@ -53,7 +64,9 @@ const Snake = () => {
         }
         return clearInterval(interval);
       }
+
       // eat apple
+
       if (snake[0] + snakeDirection === appleIndex) {
         snake.unshift(appleIndex - snakeDirection);
         grid[appleIndex].classList.remove("apple");
@@ -91,7 +104,7 @@ const Snake = () => {
       }
     }
 
-    function control(e) {
+    function handleMouseClick(e) {
       if (e.keyCode === 39) {
         snakeDirection = 1; //if we press the right arrow on our keyboard, the snake will go right one
       } else if (e.keyCode === 38) {
@@ -99,18 +112,45 @@ const Snake = () => {
       } else if (e.keyCode === 37) {
         snakeDirection = -1; // if we press left, the snake will go left one div
       } else if (e.keyCode === 40) {
-        snakeDirection = +width; //snake will go forward {width} divs, appearing to go up
+        snakeDirection = + width; //snake will go forward {width} divs, appearing to go down
+      }
+
+      if(e.keyCode === "Space" || interval === 0){
+        startGame()
       }
     }
 
-    document.addEventListener("keydown", control);
-    startButton.addEventListener("click", startGame);
+    //touch controls
+
+    function handleTouch(e){
+      e.preventDefault();
+      console.log(e.target.id)
+      switch(e.target.id){
+        case "top" : snakeDirection = - width; break;
+        case "left" : snakeDirection = -1; break;
+        case "right" : snakeDirection = 1; break;
+        case "bottom" : snakeDirection = + width; break;
+        default:
+          return;
+      }
+    }
+    
+    document.addEventListener("keydown", handleMouseClick);
+    touchControlArea.addEventListener("click", startGame);
+    touchControlArea.addEventListener("touchstart",startGame)
   });
 
   return (
     <div className="container-fluid d-flex">
       <h1 id="snakeScore">Score 0</h1>
-      <div className="gameArea" id="gameArea"></div>
+      <div className="gameArea" id="gameArea">
+      </div>
+        <div className="touchControlArea" id="touchControlArea">
+          <div className="top" id="top"></div>
+          <div className="left"id="left"></div>
+          <div className="right"id="right"></div>
+          <div className="bottom"id="bottom"></div>
+        </div>
       <button className="btn btn-success" id="startButton">
         Start
       </button>
